24d1770b2d29ee964ec2b4334e1c9a67f0fd8b4d
Gregory Avery-Weir <Gregory.Weir@gmail.com>
2023-08-17 13:09:42 -0400

    Make various usability updates and fixes

--- app/application/controller.js
+++ app/application/controller.js
@@ -5,6 +5,7 @@ import {inject as service} from '@ember/service';
 import tracery from 'tracery-grammar';
 import {Promise} from 'rsvp';
 import DS from 'ember-data';
+import {htmlSafe} from '@ember/string';
 
 export default Controller.extend({
   queryParams: ['json', 'url'],
@@ -18,7 +19,7 @@ export default Controller.extend({
   remoteError: null,
 
   overlongUrl: computed('compressed', 'compressed.isSettled', function() {
-    return this.get('compressed').then(result => result.length > 4000);
+    return DS.PromiseObject.create({promise: this.get('compressed').then(result => result.length > 4000)});
   }),
 
   resetDisabled: not('dirty'),
@@ -47,28 +48,43 @@ export default Controller.extend({
       JSON.parse(this.model);
       return DS.PromiseObject.create({promise: this.decompress.stringToZip(this.model)});
     } catch(e) {
-      return DS.PromiseObject.create({promise: new Promise((resolve, reject) => reject())});
+      return DS.PromiseObject.create({promise: Promise.reject()});
     }
   }),
 
   baseURL: computed(function() {
     let prot = location.protocol;
-    let host = location.hostname;
+    let host = location.host;
     let path = location.pathname;
+
     return `${prot}//${host}${path}`;
   }),
 
   makeShortURL() {
-    return this.compressed.then(comp => {
-      
-      return this.shortener.shorten(`${this.baseURL}?json=${encodeURIComponent(comp)}`)
-      .then(short => {
-        this.set('shortUrl', short);
-        return short;
-      }).catch(() => {
-        this.set('remoteError', `Error shortening URL. It may be too long. Try making a Gist or other remotely-hosted file and link the raw URL like ${this.baseURL}?url=[URL-component-encoded address of a grammar file]`);
+    if(location.hostname == 'localhost') {
+      this.set('remoteError', "Oops, is.gd doesn't like shortening localhost URLs. If you want to run locally, use a hosts entry so it thinks this is a real website.");
+      return Promise.reject();
+    }
+    if(this.url) {
+      return this.shortener.shorten(`${this.baseURL}?url=${encodeURIComponent(this.url)}`)
+        .then(short => {
+          this.set('shortUrl', short);
+          return short;
+        }).catch(() => {
+          this.set('remoteError', htmlSafe('Error shortening URL. Please <a href="https://github.com/GregoryAveryWeir/tracery-live/issues">report this as a bug</a> with the URL of the grammar!'));
+        });
+    } else {
+      return this.compressed.then(comp => {
+
+        return this.shortener.shorten(`${this.baseURL}?json=${encodeURIComponent(comp)}`)
+        .then(short => {
+          this.set('shortUrl', short);
+          return short;
+        }).catch(() => {
+          this.set('remoteError', `Error shortening URL. It may be too long. Try making a Gist or other remotely-hosted file and link the raw URL like ${this.baseURL}?url=[URL-component-encoded address of a grammar file]`);
+        });
       });
-    });
+    }
   },
 
   actions: {
@@ -84,6 +100,12 @@ export default Controller.extend({
     makeShortURLAction() {
       if(!this.shortUrl) {
         this.makeShortURL();
+      }
+    },
+    loadURLAction() {
+      var url = prompt("Enter a URL of a grammar as a JSON file:")
+      if(url) {
+        window.location.search = `?url=${encodeURIComponent(url)}`;
       }
     },
     saveToURL() {
